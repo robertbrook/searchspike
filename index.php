9f73499cf342f448f801ea7c73cc6a78236b279d
Robert Brook <robertbrook@fastmail.fm>
2022-01-16 17:48:51 +0000

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ $context = stream_context_create($opts);
 
 $json = file_get_contents("https://api.cognitive.microsoft.com/bing/v7.0/search?q=${q}+site:parliament.uk&count=50&textDecorations=true&textFormat=HTML", false, $context);
 $obj = json_decode($json);
-print_r($obj);
+print_r($obj->webPages->value);
 
 }
    
