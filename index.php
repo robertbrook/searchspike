90ecb284afab7c6bb987ce9e700e1e1c0dfdd9d1
Robert Brook <robertbrook@fastmail.fm>
2022-01-16 17:16:34 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php 
 
+$apikey = getenv("APIKEY");
 
-echo phpinfo();
+$q = "woking";
 
-// getenv("APIKEY");
+$opts = [
+    "http" => [
+        "method" => "GET",
+        "header" => "Ocp-Apim-Subscription-Key: ${apikey}\r\n"
+    ]
+];
+
+$context = stream_context_create($opts);
+
+$json = file_get_contents('https://api.cognitive.microsoft.com/bing/v7.0/search?q=${q}+site:parliament.uk&count=50&textDecorations=true&textFormat=HTML', false, $context);
+$obj = json_decode($json);
+print_r($obj);
+
