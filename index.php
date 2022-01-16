6c60a2b9a32b916b5fb685f57c670c28eab3c75b
Robert Brook <robertbrook@fastmail.fm>
2022-01-16 18:23:34 +0000

    Update index.php

--- index.php
+++ index.php
@@ -43,10 +43,25 @@ $obj = json_decode($json);
 
     
 foreach ($obj->webPages->value as $value) {
-    print_r($value);
+    // print_r($value);
     
+/*
+  [id] => https://api.cognitive.microsoft.com/api/v7/#WebPages.41
+    [name] => Armed Forces Bill (Division 1: held on Wednesday 8 Dec ...
+    [url] => https://hansard.parliament.uk/Lords/2021-12-08/division/0DE8BCEC-0317-4972-AD3B-CD42E7456A5C/ArmedForcesBill?outputType=Names
+    [isFamilyFriendly] => 1
+    [displayUrl] => https://hansard.<b>parliament.uk</b>/Lords/2021-12-08/division/0DE8BCEC-0317-4972-AD3B-CD42E...
+    [snippet] => Division 1: held on Wednesday 8 December 2021. Download CSV file View within context of debate. This page shows the Hansard record for the division. You can also view the division on the Votes in<b> Parliament website.</b>
+    [dateLastCrawled] => 2022-01-12T00:43:00.0000000Z
+    [language] => en
+    [isNavigational] => 
+*/
 echo <<<ARTICLE
-<article>
+<article id="$value->snippet">
+<big><a href="$value->url">$value->name</a></big>
+<br>
+$value->displayUrl
+<br>
 $value->snippet
 </article>
 ARTICLE;
