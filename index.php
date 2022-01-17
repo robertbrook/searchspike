53dfec2dccbcc78e53b2bf2309aa2425ce3a400f
Robert Brook <robertbrook@fastmail.fm>
2022-01-17 10:26:16 +0000

    Stricter check for author

--- index.php
+++ index.php
@@ -59,7 +59,9 @@ if (isset($tags['description'])) { $article_snippet = $tags['description']; }
 $article_title = $value->name;
 if (isset($tags['citation_title'])) { $article_title = $tags['citation_title']; }
 
-if (isset($tags['citation_author'])) { $article_author = "Author <strong>" . $tags['citation_author'] . "</strong>"; }
+if (
+    isset($tags['citation_author']) === true && $tags['citation_author'] === ''
+    ) { $article_author = "Author <strong>" . $tags['citation_author'] . "</strong>"; }
 
 echo <<<ARTICLE
 <article id="$value->id">
